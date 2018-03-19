ba82374f80cd81742eb611c09d0b3e2bb9a99265
Jimmy <jimmywu126@gmail.com>
2018-03-19 16:12:04 -0700

    Modify cluster-based classification to use sampled dataset instead of full dataset.

--- classify_by_cluster.py
+++ classify_by_cluster.py
@@ -37,7 +37,7 @@ argument_parser.add_argument('--dataset_type',
                              help='Which part of the dataset to evaluate, i.e. "val" (default), or "test"')
 
 
-def train_and_evaluate_k_nearest_neighbors(X_train, y_train, X_evaluation, y_evaluation):
+def train_and_evaluate_k_nearest_neighbors(X_train, y_train, X_evaluation, y_evaluation, training_sample_fraction=0.01):
     """
     Trains a multi-label k-nearest neighbors model and evaluates its performance.
 
@@ -46,8 +46,12 @@ def train_and_evaluate_k_nearest_neighbors(X_train, y_train, X_evaluation, y_eva
         y_train: (2D NumPy array) where y[i,j] is 1 if the i-th training example has label j, and 0 otherwise
         X_evaluation: (2D NumPy array) where X[i,j] is the j-th feature value for the i-th evaluation example
         y_evaluation: (2D NumPy array) where y[i,j] is 1 if the i-th evaluation example has label j, and 0 otherwise
+        training_sample_fraction: (float) the fraction of training examples to use when training
     """
     logging.info('Starting evaluation of k-nearest neighbors')
+
+    # Sample a subset of the data in a way that preserves the proportion of examples with each label
+    X_train, y_train = utils.sample_examples_by_class(X_train, y_train, training_sample_fraction)
 
     logging.info('Fitting model')
 
@@ -69,7 +73,10 @@ def train_and_evaluate_k_nearest_neighbors(X_train, y_train, X_evaluation, y_eva
     utils.print_class_accuracy(evaluation_AUROCs)
 
 
-def train_and_evaluate_multilabel_classifier_from_binary_classifier(BinaryClassifier, X_train, y_train, X_evaluation, y_evaluation, binary_classifier_arguments={}):
+def train_and_evaluate_multilabel_classifier_from_binary_classifier(
+        BinaryClassifier, X_train, y_train, X_evaluation, y_evaluation,
+        binary_classifier_arguments={},
+        training_sample_fraction=1.0):
     """
     Instantiates a multi-label model based on a given binary classifier, trains it, and evaluates its performance.
 
@@ -79,6 +86,7 @@ def train_and_evaluate_multilabel_classifier_from_binary_classifier(BinaryClassi
         y_train: (2D NumPy array) where y[i,j] is 1 if the i-th training example has label j, and 0 otherwise
         X_evaluation: (2D NumPy array) where X[i,j] is the j-th feature value for the i-th evaluation example
         y_evaluation: (2D NumPy array) where y[i,j] is 1 if the i-th evaluation example has label j, and 0 otherwise
+        training_sample_fraction: (float) the fraction of training examples to use when training
     """
     logging.info('Starting evaluation of a multi-label classifier based on ' + BinaryClassifier.__name__)
 
